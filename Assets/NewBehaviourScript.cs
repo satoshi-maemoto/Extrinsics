a61200d66a1211466c8b280db689b64ed0825e4f
satoshi-maemoto <peugeot.106.s16@gmail.com>
2022-02-26 11:39:42 +0900

    Initial push

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,23 @@
+using TMPro;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Camera cameraA;
+    public Camera cameraB;
+    public TMP_Text text;
+    public TMP_Text textUI;
+
+    void Update()
+    {
+        var T = this.cameraB.transform.position - this.cameraA.transform.position;
+        var R = Quaternion.FromToRotation(this.cameraB.transform.rotation.eulerAngles, this.cameraA.transform.rotation.eulerAngles);
+        this.text.text = 
+        $"A->B\n" +
+        $"R:({R.x:f2},{R.y:f2},{R.z:f2})\n" +
+        $"T:({T.x:f2},{T.y:f2},{T.z:f2})"
+        ;
+        this.textUI.text = this.text.text;
+    }
+}
